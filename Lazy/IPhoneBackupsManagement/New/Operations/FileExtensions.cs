f70604dde045274902fb41d9cbe9b73bdebd98ba
Arialdo Martini <arialdomartini@gmail.com>
2021-10-18 20:19:10 +0200

    Handles file duplicates adding a suffix to their names

--- Lazy/IPhoneBackupsManagement/New/Operations/FileExtensions.cs
+++ Lazy/IPhoneBackupsManagement/New/Operations/FileExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using Lazy.IPhoneBackupsManagement.Old;
 
@@ -9,9 +10,29 @@ namespace Lazy.IPhoneBackupsManagement.New.Operations
         {
             if (!dryRun)
             {
+                var mkDirP = outputDirectory.MkDirP(dryRun);
+                var destFileName = CalculateDestinationFileName(mkDirP, fileInfo);
+                
                 File.Move(fileInfo.FullName,
-                    Path.Combine(outputDirectory.MkDirP(dryRun), fileInfo.Name));
+                    destFileName);
             }
+        }
+
+        private static string CalculateDestinationFileName(string directory, FileInfo fileInfo)
+        {
+            var @base = Path.Combine(directory, fileInfo.Name);
+            if (!File.Exists(@base))
+                return @base;
+
+            int i = 1;
+            while(true)
+            {
+                var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileInfo.Name);
+                var copy = Path.Combine(directory, $"{fileNameWithoutExtension}-{i}{fileInfo.Extension}");
+                if (!File.Exists(copy))
+                    return copy;
+                i++;
+            } 
         }
     }
 }
